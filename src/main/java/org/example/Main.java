d072b309b0e4f97b4a8092882b3010c4e6c56b0b
Liudmyla Sydorenko <mila_1@ukr.net>
2023-07-16 23:14:28 +0300

    Solar System has been created

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (Planets planet : Planets.values()) {
+            System.out.println(planet);
+        }
+    }
+}
